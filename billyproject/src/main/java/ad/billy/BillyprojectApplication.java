a1e97b2e7786fdd14aa35a04cdc0a2e72f9a6a6b
drAppleCo <warlock.skt@gmail.com>
2016-11-26 11:40:05 +0100

    initial comit

---
+++ billyproject/src/main/java/ad/billy/BillyprojectApplication.java
@@ -0,0 +1,12 @@
+package ad.billy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillyprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillyprojectApplication.class, args);
+	}
+}
