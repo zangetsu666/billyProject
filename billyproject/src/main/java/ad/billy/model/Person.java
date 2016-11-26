aae109c159d651612d026f11ec62327cd9be9bfa
drAppleCo <warlock.skt@gmail.com>
2016-11-26 12:54:28 +0100

    class person created

---
+++ billyproject/src/main/java/ad/billy/model/Person.java
@@ -0,0 +1,24 @@
+package ad.billy.model;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.Id;
+
+
+
+@Entity
+public class Person {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	
+
+}
